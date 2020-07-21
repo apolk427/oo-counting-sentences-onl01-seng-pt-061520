97f6440ae07c8e4d5d1943db7f0ff7e99c1428b3
Amyre Polk <myremyre101@icloud.com>
2020-07-21 20:06:07 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,22 +1,22 @@
-
 class String
 
   def sentence?
-  self.end_with?(".")
+    self.end_with?(".")
+
   end
 
   def question?
-  self.end_with?("?")
+    self.end_with?("?")
+
   end
 
   def exclamation?
- self.end_with?("!")
+    self.end_with?("!")
+
   end
 
   def count_sentences
-    self.split(/[.!?]/).reject {|x| x.empty?}.size
-  end
+    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+    return new_array.length
 
-  def count_sentences_words
-    self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size
-end
+  end
